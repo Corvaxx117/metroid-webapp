1c6bbe816c5fb484a993db7e587818970e062c7b
Corvaxx117 <julien.amiel117@gmail.com>
2025-02-19 16:48:41 +0100

    added config file

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+// Définition de l'URL de base
+define('APP_BASE_URL', $_ENV['APP_BASE_URL'] ?? $_SERVER['APP_BASE_URL'] ?? 'http://localhost:8888/FORMATION_OCR/Developpez_sites_PHP_MVC_amiel_julien/exercice_POO/public');
